9fff4079bf7b1591d032c4b78e2dc529f07d2f95
mjh39088 <gjalswo3908@gmail.com>
2022-04-22 23:20:48 +0900

    Initial commit

---
+++ src/Human.java
@@ -0,0 +1,22 @@
+public class Human {
+    public static void main(String[] args) {
+        // 프로그램의 실행 시작 지점
+        System.out.print("Hello World!\n"); // "" 은 문자열(String)을 의미함
+        System.out.print("홍드로이드 만세!\n");
+        // 변수 (변할 수 있는 값 또는 수)
+        // 자료형 String (문자열) , int (정수 숫자), boolean ( 참 or 거짓 )
+        String a = "a 변수";
+        System.out.print(a);
+        a = "a 변수 2222";
+        System.out.print(a);
+
+        int aInt = 10;
+        System.out.print(aInt);
+
+        // 불리언 변수는 반드시 true(진실) false(거짓) 만 출력이 됩니다!
+        boolean isApple = true;
+        System.out.print(isApple);
+
+        // if = 만약.. ~라면...
+    }
+}
